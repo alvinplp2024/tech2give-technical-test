39adc67f217b0f277a13f880238c74f25add3e64
alvin ondieki <159621268+alvinplp2024@users.noreply.github.com>
2024-10-26 22:42:51 +0300

    Create q5_capitalizewords.py

---
+++ q5_capitalizewords.py
@@ -0,0 +1,9 @@
+#QUESTION 5
+def capitalize_words(text):
+    # I will Use the title() method to capitalize the first letter of each word
+    return text.title()
+
+# Test cases
+print(capitalize_words("hi"))# Hi
+print(capitalize_words("i love programming"))# I Love Programming
+print(capitalize_words("john is a good man despite being angry"))#John Is A Good Man Despite Being Angry
